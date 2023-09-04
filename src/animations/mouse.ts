4a2f314367244badf70af056b0b0a7ad7a77cdf4
g4knr <85592362+g4knr@users.noreply.github.com>
2023-09-04 13:07:27 +0100

    Updates

--- src/animations/mouse.ts
+++ src/animations/mouse.ts
@@ -5,10 +5,17 @@ export const mouse = () => {
   const mouse = queryElement<HTMLDivElement>('.mouse-cursor');
   if (!mouse) return;
 
+  const mouseText = queryElement<HTMLDivElement>('[data-mouse="text"]', mouse);
+
   const elements = queryElements<HTMLElement>('a, [href], [data-mouse-class]');
   elements.forEach((element) => {
     const cursorClass = element.dataset.mouseClass ?? 'cc-click';
+
+    console.log(element, cursorClass);
+
     element.addEventListener('mouseenter', () => {
+      const text = element.dataset.mouseText ?? 'View Case';
+      if (mouseText) mouseText.textContent = text;
       mouse.classList.add(cursorClass);
     });
 
--- src/pages/cases.ts
+++ src/pages/cases.ts
@@ -1,31 +1,42 @@
-import { Splide } from '@splidejs/splide';
+// import { Splide } from '@splidejs/splide';
+
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 import { queryElement } from '$utils/queryElement';
+import { queryElements } from '$utils/queryElements';
 
 export const cases = () => {
   console.log('cases');
-  const component = queryElement<HTMLDivElement>('.splide.cc-cases');
+
+  const component = queryElement<HTMLDivElement>('.cases');
   if (!component) return;
 
-  const slider = new Splide(component, {
-    arrows: false,
-    pagination: false,
-    wheel: true,
-    autoWidth: true,
+  const track = queryElement<HTMLDivElement>('.cases-track', component);
+  const cases = queryElements<HTMLDivElement>('.cases-item', component);
+
+  if (!track || !cases) return;
+
+  const length = (cases.length - 2) * cases[0].offsetWidth;
+
+  const mm = gsap.matchMedia();
+  mm.add('(min-width: 992px)', () => {
+    component.style.height = `${window.innerHeight + length}px`;
+    const timeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: component,
+        start: 'top top',
+        end: 'bottom bottom',
+        scrub: true,
+      },
+    });
+
+    timeline.to(track, {
+      x: -length,
+    });
+
+    return () => {
+      component.style.removeProperty('height');
+    };
   });
-
-  const mediaQuery = window.matchMedia('(min-width: 991px)');
-  if (mediaQuery.matches) {
-    slider.mount();
-  }
-  mediaQuery.addEventListener('change', handleBreakpointChange);
-
-  // Function to handle breakpoint changes
-  function handleBreakpointChange(event: MediaQueryListEvent) {
-    if (event.matches) {
-      slider.mount();
-    } else {
-      slider.destroy();
-    }
-  }
 };
